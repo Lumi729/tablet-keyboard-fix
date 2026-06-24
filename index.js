ade8884586e75509eab3675590849ce1f578e249
Lumi729 <1579698669@qq.com>
2026-06-24 21:29:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 (function () {
     try {
-        // 键盘修复
         var vp = document.querySelector('meta[name="viewport"]');
         if (vp && !vp.content.includes('interactive-widget')) {
             vp.content += ', interactive-widget=resizes-content';
@@ -9,22 +8,6 @@
             setTimeout(function () {
                 window.scrollTo(0, 0);
             }, 100);
-        });
-
-        // 点击屏幕顶部区域时，抓那个位置的元素信息
-        document.addEventListener('click', function (e) {
-            if (e.clientY < 80) {
-                var el = document.elementFromPoint(e.clientX, e.clientY);
-                if (el) {
-                    var info = [];
-                    var cur = el;
-                    while (cur && cur !== document.body) {
-                        info.push(cur.tagName + (cur.id ? '#' + cur.id : '') + (cur.className ? '.' + String(cur.className).split(' ').join('.') : ''));
-                        cur = cur.parentElement;
-                    }
-                    alert(info.join('\n'));
-                }
-            }
         });
     } catch (e) {}
 })();
